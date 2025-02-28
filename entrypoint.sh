a7f1c3bd7f1b7a7e9255df787f5acdded1905651
uTkArShVeRm4 <theutkarsh.verma00@gmail.com>
2025-03-01 00:10:09 +0530

    uhuhuhuhuhuu

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 
-# Ensure SSH agent is running
-eval $(ssh-agent -s)
+# Create a temporary askpass script
+echo "#!/bin/sh\necho \$GIT_ACCESS_TOKEN" >/git-askpass.sh
+chmod +x /git-askpass.sh
 
-# Add SSH key (mounted from the host)
-ssh-add /root/.ssh/id_ed25519
+# Tell Git to use the askpass script
+export GIT_ASKPASS=/git-askpass.sh
 
-# Configure Git
+# Set up Git config
 git config --global user.name "$GIT_USERNAME"
 git config --global user.email "$GIT_EMAIL"
-git config --global --add safe.directory /app/data/Main
 
-# Make sure the repo is using SSH instead of HTTPS
-if [ -n "$GIT_REMOTE_URL" ]; then
-    git -C /app/data/Main remote set-url origin "$GIT_REMOTE_URL"
-fi
+# Set remote URL without exposing credentials
+git -C /app/data/Main remote set-url origin "https://github.com/$GIT_USERNAME/Main.git"
 
-# Execute the main application
+# Run the main application
 exec ./main
