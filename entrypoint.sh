4b6d525fb52792370afda89e9814468fa3bb84d3
uTkArShVeRm4 <theutkarsh.verma00@gmail.com>
2025-02-28 23:14:45 +0530

    more perms

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,30 +5,23 @@ if [ -n "$GIT_USERNAME" ] && [ -n "$GIT_EMAIL" ]; then
     git config --global user.name "$GIT_USERNAME"
     git config --global user.email "$GIT_EMAIL"
 
-    # If using a personal access token
     if [ -n "$GIT_ACCESS_TOKEN" ]; then
-        # Configure git to store credentials
         git config --global credential.helper store
-
-        # Create or update .git-credentials file
-        echo "https://$GIT_USERNAME:$GIT_ACCESS_TOKEN@github.com" >/app/data/.git-credentials
-        git config --global credential.helper "store --file=/app/data/.git-credentials"
-
-        # Set file permissions
-        chmod 600 /app/data/.git-credentials
+        echo "https://$GIT_USERNAME:$GIT_ACCESS_TOKEN@github.com" >/root/.git-credentials
+        git config --global credential.helper "store --file=/root/.git-credentials"
+        chmod 600 /root/.git-credentials
     fi
 
-    # Additional configurations
-    git config --global --add safe.directory /app/data
+    git config --global --add safe.directory /app/data/Main
 fi
 
 # If remote URL is provided, add it
 if [ -n "$GIT_REMOTE_URL" ]; then
-    git remote add origin "$GIT_REMOTE_URL"
+    git -C /app/data/Main remote add origin "$GIT_REMOTE_URL"
 fi
 
 # Fix ownership issues
-chown -R root:root /app/data/.git
+chown -R root:root /app/data/Main/.git
 
 # Execute the main application
 exec ./main
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 sudo docker run -d -p 7777:7777 \
     -v $(pwd)/data:/app/data itt \
+    -v ~/.git-credentials:/root/.git-credentials \
     -e GIT_USERNAME="your-username" \
     -e GIT_EMAIL="your-email@example.com" \
     -e GIT_ACCESS_TOKEN="your-personal-access-token" \
