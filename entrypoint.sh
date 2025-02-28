292e13baedd0e765816377c4681eb8afbb39b4fd
uTkArShVeRm4 <theutkarsh.verma00@gmail.com>
2025-02-28 23:28:33 +0530

    fuck docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,21 @@ if [ -n "$GIT_USERNAME" ] && [ -n "$GIT_EMAIL" ]; then
     git config --global user.name "$GIT_USERNAME"
     git config --global user.email "$GIT_EMAIL"
 
+    # If using a personal access token
     if [ -n "$GIT_ACCESS_TOKEN" ]; then
+        # Configure git to store credentials
         git config --global credential.helper store
-        echo "https://$GIT_USERNAME:$GIT_ACCESS_TOKEN@github.com" >/root/.git-credentials
-        git config --global credential.helper "store --file=/root/.git-credentials"
-        chmod 600 /root/.git-credentials
+
+        # Create or update .git-credentials file
+        echo "https://$GIT_USERNAME:$GIT_ACCESS_TOKEN@github.com" >/app/data/.git-credentials
+        git config --global credential.helper "store --file=/app/data/.git-credentials"
+
+        # Set file permissions
+        chmod 600 /app/data/.git-credentials
     fi
 
-    git config --global --add safe.directory /app/data/Main
+    # Additional configurations
+    git config --global --add safe.directory /app/data
 fi
 
 # If remote URL is provided, add it
